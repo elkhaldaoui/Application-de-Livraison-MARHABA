a10cfe227214933e95542b0edc791f2e1429355f
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-11-04 10:58:13 +0100

    auth with react partie password vrify

--- config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const db = mongoose.connect(process.env.DB_URL, ()=>console.log('db connected'))
-
-
-module.exports = db
--- routes/role-router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const verifyRole = require('../middelware/verifyRole')
-
-router.get('/client/me', verifyRole.verifyRoleClient)
-router.get('/Livreur/me', verifyRole.verifyRoleLivreur)
-router.get('/manager/me', verifyRole.verifyRoleManager)
-
-module.exports = router
