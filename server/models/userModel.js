c7fdfe47cfdc738da102bea48750e3ebc8ae5eb6
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-11-22 21:32:13 +0100

    ...

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true},
-    passwordHash: {type: String, required: true},
-})
+  email: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+});
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("user", userSchema);
 
-module.exportes = User;
-
+module.exports = User;
