8ce9a54ace136fa38888e462f131558339395f2b
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-11-30 08:48:41 +0100

    la realisation de partie test avec jest et supertest

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,21 +1,7 @@
 const supertest = require("supertest");
 const app = require("./server");
 
-describe("register", ()=>{
-    test("Please fill all the fields", async()=>{
-    const body = {
-        username: "",
-        email: "",
-        password: ""
-    }
-        const res = await supertest(app).post('/api/auth/register').send({
-            username:'',
-            email:'',
-            password:''
-        })
-        expect(res.statusCode).toBe(400)
-    })
-})
+
 
 
 describe('POST Login', () => {
